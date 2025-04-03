6655472202464403ba83f6682b2687bc4ea936a4
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2025-04-03 13:54:29 +0200

    felt weird not having dishes not be sorted by category, so fixed that real quick, kind of an excuse to work on the deployment process aswell

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,10 +2,21 @@ import Dish from "./Dish";
 import styles from "./Menu.module.css";
 
 function Menu({ dishes }) {
+  const categories = ["Forrett", "Hovedrett", "Dessert"];
+
   return (
-    <div className={styles.grid}>
-      {dishes.map((dish) => (
-        <Dish key={dish.id} dish={dish} />
+    <div>
+      {categories.map((category) => (
+        <div key={category}>
+          <h2>{category}</h2>
+          <div className={styles.grid}>
+            {dishes
+              .filter((dish) => dish.kategori === category)
+              .map((dish) => (
+                <Dish key={dish.id} dish={dish} />
+              ))}
+          </div>
+        </div>
       ))}
     </div>
   );
