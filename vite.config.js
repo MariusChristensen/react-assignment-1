602a9a31ac9e0e9a2f2012c9dafb986f1bf8d6fc
MariusChristensen <91680871+MariusChristensen@users.noreply.github.com>
2025-04-03 13:29:39 +0200

    fixing deployment to github pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.com/MariusChristensen/react-assignment-1",
+  base: "/react-assignment-1/",
 });
